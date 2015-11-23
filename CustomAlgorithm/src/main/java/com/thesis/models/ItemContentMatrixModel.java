b97ef13614267db5f33b7a5e452898a24504828d
LucaNardulli <LucaNardulli@192.168.0.2>
2015-11-23 10:32:34 +0100

    - Aggiunto qualifier ItemContentMatrixModel - Bozza classe ItemContentMatrixModel

---
+++ CustomAlgorithm/src/main/java/com/thesis/models/ItemContentMatrixModel.java
@@ -0,0 +1,40 @@
+package com.thesis.models;
+import java.io.Serializable;
+import java.util.Map;
+import org.grouplens.grapht.annotation.DefaultProvider;
+import org.grouplens.lenskit.collections.LongUtils;
+import org.grouplens.lenskit.core.Shareable;
+import org.grouplens.lenskit.knn.item.model.ItemItemModel;
+import org.grouplens.lenskit.vectors.ImmutableSparseVector;
+import org.grouplens.lenskit.vectors.SparseVector;
+import it.unimi.dsi.fastutil.longs.LongSortedSet;
+
+/**
+ * Item content similarity model using a normalized in-memory similarity matrix.
+ */
+@DefaultProvider(ItemContentMatrixModelBuilder.class)
+@Shareable
+public class ItemContentMatrixModel implements Serializable, ItemItemModel {
+	private static final long serialVersionUID = 1L;
+
+	private final Map<Long,ImmutableSparseVector> model;
+
+	public ItemContentMatrixModel(Map<Long,ImmutableSparseVector> nbrs) {
+		model = nbrs;
+	}
+
+
+	@Override
+	public LongSortedSet getItemUniverse() {	
+		return LongUtils.packedSet(model.keySet());
+	}
+
+
+	@Override
+	public SparseVector getNeighbors(long item) {
+		if (model.containsKey(item))
+			return model.get(item);
+		else
+			return ImmutableSparseVector.empty();
+	}
+}
---
+++ CustomAlgorithm/src/main/java/com/thesis/models/ItemContentMatrixModelBuilder.java
@@ -0,0 +1,131 @@
+package com.thesis.models;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.concurrent.NotThreadSafe;
+import javax.inject.Inject;
+import javax.inject.Provider;
+
+import org.grouplens.lenskit.core.Transient;
+import org.grouplens.lenskit.knn.item.model.ItemItemBuildContext;
+import org.grouplens.lenskit.knn.item.model.ItemItemModel;
+import org.grouplens.lenskit.util.ScoredItemAccumulator;
+import org.grouplens.lenskit.util.UnlimitedScoredItemAccumulator;
+import org.grouplens.lenskit.vectors.ImmutableSparseVector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Stopwatch;
+
+import it.unimi.dsi.fastutil.longs.Long2ObjectMap;
+import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
+import it.unimi.dsi.fastutil.longs.LongBidirectionalIterator;
+import it.unimi.dsi.fastutil.longs.LongSet;
+import it.unimi.dsi.fastutil.longs.LongSortedSet;
+
+/**
+ * Build a item content similarity model.
+ */
+@NotThreadSafe
+public class ItemContentMatrixModelBuilder implements Provider<ItemItemModel> {
+	private static final Logger logger = LoggerFactory.getLogger(ItemContentMatrixModelBuilder.class);
+
+	private final ItemItemBuildContext context;
+
+
+	@Inject
+	public ItemContentMatrixModelBuilder(@Transient ItemItemBuildContext context) {
+		this.context = context;
+	}
+
+
+	@Override
+	public ItemItemModel get() {
+
+		LongSortedSet allItems = context.getItems();
+		int nitems = allItems.size();
+
+		HashMap<Long,String> icMap = getItemsContentMap();
+		
+		logger.info("building item-content similarity model for {} items", nitems);
+		logger.info("item-content similarity model is symmetric");
+
+		Long2ObjectMap<ScoredItemAccumulator> rows = makeAccumulators(allItems);
+
+		Stopwatch timer = Stopwatch.createStarted();
+		int ndone=1;
+		for(LongBidirectionalIterator itI = allItems.iterator(); itI.hasNext() ; ) {
+			Long i = itI.next();
+			
+			if (logger.isDebugEnabled()) 
+				logger.info("computing similarities for item {} ({} of {})", i, ndone, nitems);
+
+			for(LongBidirectionalIterator itJ = allItems.iterator(i); itJ.hasNext(); ) {
+				Long j = itJ.next();
+				
+				String contentI = icMap.get(i);
+				String contentJ = icMap.get(j);
+				
+				double simIJ = 0;	// trova similarità tra contentI e contentJ
+			
+				rows.get(i).put(j, simIJ);
+				rows.get(j).put(i, simIJ);
+			}
+
+			if (logger.isDebugEnabled() && ndone % 100 == 0) 
+				logger.info("computed {} of {} model rows ({}s/row)", 
+						ndone, nitems, 
+						String.format("%.3f", timer.elapsed(TimeUnit.MILLISECONDS) * 0.001 / ndone));
+
+			ndone++;
+		}
+		
+		timer.stop();
+		logger.info("built model for {} items in {}", ndone, timer);
+
+		return new ItemContentMatrixModel(finishRows(rows));
+	}
+
+	private HashMap<Long,String> getItemsContentMap(){
+		HashMap<Long,String> icMap = new HashMap<Long,String>();
+		for(long item : context.getItems()){
+			String content = "";
+			try {
+				content = readItemContent(item);
+			}catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			icMap.put(item, content);
+		}
+		return icMap;
+	}
+		
+	private String readItemContent(long item) throws IOException{
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new FileReader(new File(System.getProperty("user.dir")+"/CustomAlgorithm/data/abstract/"+item)));
+		String s = "";
+		while((s = br.readLine()) != null)
+			sb.append(s);
+		br.close();
+		return sb.toString();
+	}
+
+	private Long2ObjectMap<ScoredItemAccumulator> makeAccumulators(LongSet items) {
+		Long2ObjectMap<ScoredItemAccumulator> rows = new Long2ObjectOpenHashMap<ScoredItemAccumulator>(items.size());
+		for(Long item : items)
+			rows.put(item, new UnlimitedScoredItemAccumulator());       
+		return rows;
+	}
+
+	private Long2ObjectMap<ImmutableSparseVector> finishRows(Long2ObjectMap<ScoredItemAccumulator> rows) {
+		Long2ObjectMap<ImmutableSparseVector> results = new Long2ObjectOpenHashMap<ImmutableSparseVector>(rows.size());
+		for (Long2ObjectMap.Entry<ScoredItemAccumulator> e: rows.long2ObjectEntrySet()) 
+			results.put(e.getLongKey(), e.getValue().finishVector().freeze());      
+		return results;
+	}
+}
